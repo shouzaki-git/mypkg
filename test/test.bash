63600634478c80d25eb9d43aac607cdc33bbd8c6
Sho Uzaki <sho.uzaki@icloud.com>
2023-12-22 13:52:52 +0900

    fixed test

--- test/test.bash
+++ test/test.bash
@@ -5,6 +5,8 @@ dir=~
 
 cd $dir/ros2_ws
 colcon build
+source $dir/ros2_ws/install/setup.bash
+source $dir/ros2_ws/install/local_setup.bash
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
